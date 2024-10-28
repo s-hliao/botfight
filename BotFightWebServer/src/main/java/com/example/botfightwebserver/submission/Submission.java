97755abd0b4c0972c686794eff277e3aef3f2fa6
Tkwok7 <tkwok7@gatech.edu>
2024-10-28 11:02:28 -0400

    edited schema

--- BotFightWebServer/src/main/java/com/example/botfightwebserver/submission/Submission.java
+++ BotFightWebServer/src/main/java/com/example/botfightwebserver/submission/Submission.java
@@ -9,11 +9,15 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.PrePersist;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
 
 enum SUBMISSION_VALIDITY {
     VALID,
@@ -36,5 +40,13 @@ public class Submission {
 
     @Enumerated(EnumType.STRING)
     private SUBMISSION_VALIDITY submissionValidity;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @PrePersist
+    protected void onCreate() {
+        createdAt = LocalDateTime.now();
+    }
 
 }
